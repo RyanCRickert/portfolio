a7b6998e0784228dab7ef949f278548f7426775d
Ryan Rickert <RyanCRickert@gmail.com>
2018-05-13 01:58:46 -0500

    Updated skills

--- src/skills/skills.js
+++ src/skills/skills.js
@@ -1,7 +1,8 @@
 export default [
   "Version control from using GitHub",
   "Website Deployment using Heroku/GitHub",
-  "Using React/Redux to create interaction throughout a website",
+  "Using React/Redux to manage state throughout a website",
+  "Using Thunk middleware for asynchronous calls to database",
   "Writing test cases for various application needs",
   "Using Ajax calls to an API (weather widget in the footer)",
   "Using two types of databases, Firebase and MongoDB",
